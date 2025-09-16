09ca11aef9faf5357c1975261c9944e5b0709096
Andressa Lorena Marinho <100449391+MarinhoAndressa@users.noreply.github.com>
2025-09-15 21:14:52 -0400

    Add files via upload

---
+++ Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiServico
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public required string Descricao { get; set; }
+        public DateOnly DataAbertura { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public DateOnly Fechamento { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public string Status { get; set; } = "Aberto";
+    }
+}
+
